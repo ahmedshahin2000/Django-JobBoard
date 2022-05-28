b1e90b9b2c3223cc83665a2545acf6bfd8a6da81
ahmedshahin2000 <42018024@hti.edu.eg>
2022-05-28 22:24:14 +0200

    add job models

--- job/models.py
+++ job/models.py
@@ -1,3 +1,30 @@
+
 from django.db import models
 
-# Create your models here.
+
+'''
+django model field
+    - HTML Widget
+    - Validition
+    - db Size
+'''
+
+
+JOB_TYPE = (
+    ('Full Time','Full Time'),
+    ('Part Time','Part Time'),
+)
+
+class Job(models.Model): # Table
+    
+    title = models.CharField(max_length=50) # Column
+    # location
+    job_type = models.CharField(max_length=15, choices=JOB_TYPE)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experience = models.IntegerField(default=1)
+    
+    def __str__(self):
+        return self.title
